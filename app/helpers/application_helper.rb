071bb1ff96d4b57ee3f546ff587ffcb4a02f60ca
Aaron Eisenberger <aaron@Aarons-MacBook-Pro.local>
2019-02-07 17:34:08 +0700

    flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,12 @@ module ApplicationHelper
     resource.persisted? ? resource_path(resource) : collection_path(parent)
   end
 
+  def flash_tag
+    if flash[:notice] || flash[:alert]
+      content_tag(:div, flash[:notice] || flash[:alert], class: "alert alert-#{flash[:notice] ? 'success' : 'danger'} alert-dismissible fade show m-0", role: "alert")
+    end
+  end
+
   def menu_item(text, path, icon)
     content = icon_tag(icon) + content_tag(:span, text)
     active_link_to content, path, class: 'nav-link', wrap_tag: :li, wrap_class: 'nav-item'
