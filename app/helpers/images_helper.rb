ba200558035d53591139cc7dc696ad73b83a0b26
Aaron Eisenberger <aaron@Aarons-MacBook-Pro.local>
2019-02-08 18:45:53 +0700

    fix

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,7 +1,8 @@
 module ImagesHelper
 
   def tmdb_image_tag(media, type, options = {})
-    image_tag media.tmdb_image_path(type), options.merge(alt: media.name)
+    image_path = media.tmdb_image_path(type)
+    image_tag image_path, options.merge(alt: media.name) if image_path
   end
 
 end
